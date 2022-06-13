78cd1274e80658b5bc407d2a0c90418561d41c79
Stinckygoo <91928322+Stinckygoo@users.noreply.github.com>
2022-06-13 11:33:10 +0800

    0613

--- app/src/main/java/com/example/game2/Ball_SurfaceView.kt
+++ app/src/main/java/com/example/game2/Ball_SurfaceView.kt
@@ -5,14 +5,17 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import kotlin.random.Random
 
-class BallSurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),SurfaceHolder.Callback2 {
+class BallSurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
+    SurfaceHolder.Callback2 {
+    var myPaint = Paint()
     var surfaceHolder: SurfaceHolder = holder
     var Ball: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.ball)
-    private var ballOrigX : Int = 0
-    private var ballOrigY : Int = 0
-    private var ballMoveX : Int = 4
-    private var ballMoveY : Int = 4
+    private var ballOrigX: Int = 0
+    private var ballOrigY: Int = 0
+    private var ballMoveX: Int = 8
+    private var ballMoveY: Int = 8
 
 
     init {
@@ -27,16 +30,32 @@ class BallSurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(con
     }
 
     fun drawSomething(canvas: Canvas) {
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
         ballOrigX += ballMoveX
         ballOrigY += ballMoveY
 //        canvas.drawBitmap(Ball, 500f, 500f, null)
-        if (ballOrigX>width-Ball.width || ballOrigX<= 0){
+        if (ballOrigX > width - Ball.width || ballOrigX <= 0) {
             ballMoveX *= -1
+            ballMoveY = if (ballMoveY >0 ){
+                rand(7,12)
+            }else{
+                rand(7,12)*(-1)
+            }
+
+
         }
-        if (ballOrigY>height-Ball.height || ballOrigY<= 0){
-            ballMoveY *= -1
+        if (ballOrigY > height - Ball.height || ballOrigY <= 0) {
+            ballMoveY *=- 1
+            ballMoveX = if (ballMoveX >0 ){
+                rand(7,12)
+            }else{
+                rand(7,12)*(-1)
+            }
+
         }
+        canvas.drawRGB(255,228,225)
         canvas.drawBitmap(Ball, ballOrigX.toFloat(), ballOrigY.toFloat(), null)
+
 
     }
 
@@ -50,5 +69,9 @@ class BallSurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(con
 
     override fun surfaceRedrawNeeded(p0: SurfaceHolder) {
 
+    }
+    private fun rand(start: Int, end: Int): Int {
+        require(start <= end) { "Illegal Argument" }
+        return (start..end).random()
     }
 }
