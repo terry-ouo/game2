f4efa894d25b38f2bc7f90e9405fe2ae7a4de139
Stinckygoo <91928322+Stinckygoo@users.noreply.github.com>
2022-06-15 23:35:35 +0800

    ok

--- app/src/main/java/com/example/game2/MainActivity.kt
+++ app/src/main/java/com/example/game2/MainActivity.kt
@@ -17,10 +17,10 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener,
     lateinit var binding: ActivityMainBinding
     private var gameStatus = false
     private var gameEnd = false
-    var finalscore: Int=0
+    var finalscore: Int = 0
     var paddlePosition: Int = 0
-    var secondsCount:Int = 0    //計時
-    var statecheck:Boolean=false
+    var secondsCount: Int = 0    //計時
+    var statecheck: Boolean = false
     var db = FirebaseFirestore.getInstance()
     var user: MutableMap<String, Any> = HashMap()
     lateinit var gDetector: GestureDetector
@@ -41,11 +41,11 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener,
 
         //上傳使用者資料
         binding.btnUpdate.setOnClickListener {
-            if(binding.user.text.toString() == ""){
+            if (binding.user.text.toString() == "") {
                 Toast.makeText(this, "請輸入挑戰者名稱!", Toast.LENGTH_SHORT).show()
-            }else {
-                var score: Int= Int.MAX_VALUE
-                if(statecheck) {
+            } else {
+                var score: Int = Int.MAX_VALUE
+                if (statecheck) {
 
                     db.collection("Users")
                         .whereEqualTo("使用者名稱", binding.user.text.toString())
@@ -54,9 +54,9 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener,
                             if (task.isSuccessful) {
                                 var msg: String = "查詢結果為:\n"
                                 for (document in task.result!!) {
-                                    score=document.data["時間"].toString().toInt()
+                                    score = document.data["時間"].toString().toInt()
                                 }
-                                if(finalscore<score) {
+                                if (finalscore < score) {
                                     user["使用者名稱"] = binding.user.text.toString()
                                     user["時間"] = finalscore
                                     db.collection("Users")
@@ -64,7 +64,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener,
                                         .set(user)
                                         .addOnSuccessListener {
                                             Toast.makeText(
-                                                this, "上傳資料成功!\n成績為:"+finalscore,
+                                                this, "上傳資料成功!\n成績為:" + finalscore,
                                                 Toast.LENGTH_SHORT
                                             ).show()
                                         }
@@ -74,13 +74,13 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener,
                                                 Toast.LENGTH_SHORT
                                             ).show()
                                         }
-                                }else{
+                                } else {
                                     Toast.makeText(this, "你之前的成績更加優秀呢!", Toast.LENGTH_SHORT).show()
                                 }
                             }
                         }
 
-                }else{
+                } else {
                     Toast.makeText(this, "只有挑戰成功才能上傳成績喔!", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -89,19 +89,16 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener,
 
 
     fun gameStatus(p0: View?) {
-        binding.button.text = "暫停"
         gameStatus = !gameStatus    //=0
-        finalscore=0
+        finalscore = 0
         GlobalScope.launch(Dispatchers.Main) {
-            while (gameStatus) {    //遊戲開始 = 0
+            while (gameStatus) {
+                binding.test.text = binding.ball.ballSpeed_min.toString()
                 //計時
                 secondsCount++
-                binding.time.text = (secondsCount/35).toString()
+                binding.time.text = (secondsCount / 35).toString()
                 //遊戲結束判斷
-                if (binding.ball.countBrick == 0) {
-                    gameStatus = false
-                }
-                if (gameEnd) {
+                if (binding.ball.countBrick == 0 || gameEnd) {
                     gameStatus = false
                 }
                 delay(25)
@@ -114,37 +111,40 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener,
                         (binding.ball.ballSpeed_min..binding.ball.ballSpeed_max).random() * (-1)
                 }
                 if (binding.ball.ballOrigY > 1300) {    //球觸底
-                    gameStatus = !gameStatus    //結束
+                    gameStatus = false    //結束
                     gameEnd = true
-                    statecheck=false
-                }else if(binding.ball.countBrick == 0){//方塊全破壞
-                    gameStatus = !gameStatus    //結束
-                    finalscore=secondsCount/35
+                    statecheck = false
+                } else if (binding.ball.countBrick == 0) {//方塊全破壞
+                    gameStatus = false   //結束
+                    finalscore = secondsCount / 35
                     gameEnd = true
-                    statecheck=true
+                    statecheck = true
+                }
+                if (gameEnd && !gameStatus) {   //2
+                    binding.button.text = "重新開始"
+                    binding.ball.ballSpeed_min = 8
+                    binding.ball.ballSpeed_min = 15
+                    binding.ball.ballOrigX = 400
+                    binding.ball.ballOrigY = 1000
+                    binding.ball.ballMoveX = 0
+                    binding.ball.ballMoveY = 8
+                    binding.ball.detectBrick1 = true
+                    binding.ball.detectBrick2 = true
+                    binding.ball.detectBrick3 = true
+                    binding.ball.detectBrick4 = true
+                    binding.ball.detectBrick5 = true
+                    binding.ball.detectBrick6 = true
+                    binding.ball.countBrick = 6
+                    secondsCount = 0
+                    gameEnd = !gameEnd
+                } else if (!gameEnd && gameStatus) {
+                    binding.button.text = "暫停"
+                } else if (!gameEnd && !gameStatus) {
+                    binding.button.text = "繼續"
                 }
 
             }
-            if (gameEnd) {   //2
-                binding.button.text = "重新開始"
-                binding.ball.ballSpeed_min = 8
-                binding.ball.ballSpeed_min = 15
-                binding.ball.ballOrigX = 400
-                binding.ball.ballOrigY = 1000
-                binding.ball.ballMoveX = 0
-                binding.ball.ballMoveY = 8
-                binding.ball.detectBrick1 = true
-                binding.ball.detectBrick2 = true
-                binding.ball.detectBrick3 = true
-                binding.ball.detectBrick4 = true
-                binding.ball.detectBrick5 = true
-                binding.ball.detectBrick6 = true
-                binding.ball.countBrick = 6
-                secondsCount = 0
-                gameEnd = !gameEnd
-            } else {
-                binding.button.text = "繼續"
-            }
+
         }
     }
 
